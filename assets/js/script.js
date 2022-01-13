a5a025145bcffa970f8c73bc31fec1e1fb9864e4
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-13 13:31:13 +0800

    Add validation email function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,11 @@ const emailInput = document.getElementById("email");
  * Content onLoad
  */
 document.addEventListener('DOMContentLoaded', function () {
+  // Listen for submit button click
   submissionForm.addEventListener('submit', function (e) {
     submission(e);
   });
+  // Listen for Enter press down on submission
   document.addEventListener('keydown', function (e) {
     if (e.key === 'Enter') {
       submission(e);
@@ -19,6 +21,18 @@ document.addEventListener('DOMContentLoaded', function () {
 const submission = function (e) {
   e.preventDefault();
   const email = emailInput.value;
-  console.log(email)
 
 }
+
+/**
+ * Function to validate email input
+ * Posted on Stack overflow:
+ * https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+ */
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
