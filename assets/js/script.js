7a5ffa6e5a048f482270b7e78d465b668938e842
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-13 13:16:03 +0800

    Add js variables

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Variables
+const submissionForm = document.getElementById("form");
+const emailInput = document.getElementById("email");
+
+/**
+ * Content onLoad
+ */
+document.addEventListener('DOMContentLoaded', function () {
+
+});
