afb36b22af2e6f046b809ba3f926fa0c402537b3
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-13 13:26:45 +0800

    Add eventlisteners for submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,19 @@ const emailInput = document.getElementById("email");
  * Content onLoad
  */
 document.addEventListener('DOMContentLoaded', function () {
-
+  submissionForm.addEventListener('submit', function (e) {
+    submission(e);
+  });
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+      submission(e);
+    }
+  });
 });
+
+const submission = function (e) {
+  e.preventDefault();
+  const email = emailInput.value;
+  console.log(email)
+
+}
